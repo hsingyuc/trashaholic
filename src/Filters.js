90405a28332d7f31555873ff5f27145d6420b96c
Hsing Yu <hsingyuc.7@gmail.com>
2020-04-03 14:56:35 +0300

    Add drop down

--- src/Filters.js
+++ src/Filters.js
@@ -15,9 +15,23 @@ class Filters extends React.Component {
     render() {
         return(
             <div className='filters'>
-                <input type="text" onChange={ event => this.matchStartTime( event ) } value={ this.props.startTime } placeholder='1700' />
-                <input type="text" onChange={ event => this.matchEndTime( event ) } value={ this.props.endTime } placeholder='1800'/>
+                <label for="start-time">Start time:</label>
+                <select id="start-time" onChange={ event => this.matchStartTime( event ) } value={ this.props.startTime }>
+                    <option value="17:00">17:00</option>
+                    <option value="17:30">17:30</option>
+                    <option value="18:00">18:00</option>
+                    <option value="18:30">18:30</option>
+                </select>
+                <label for="end-time">End time:</label>
+                <select id="end-time" onChange={ event => this.matchEndTime( event ) } value={ this.props.endTime }>
+                    <option value="1730">17:30</option>
+                    <option value="1800">18:00</option>
+                    <option value="1830">18:30</option>
+                    <option value="1900">19:00</option>
+                </select>
             </div>
+
+
         );
     }
 }
