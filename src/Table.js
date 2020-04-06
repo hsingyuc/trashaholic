d0c52562fcfedbac0b608b8e2db36d104005e67b
Hsing Yu <hsingyuc.7@gmail.com>
2020-04-06 17:36:53 +0300

    Add table style

--- src/Table.js
+++ src/Table.js
@@ -6,11 +6,16 @@ class Table extends React.Component {
         return this.props.collectionPlaces.map( ( data, index ) => {
             const { distance, startTime, endTime, address } = data;
 
-            return <tr key={ index }>
-                    <td>{ startTime }-{ endTime }</td>
-                    <td>{ address }</td>
-                    <td>{ this.getFormattedDistance(distance) }</td>
-                </tr>
+            return  <li key={ index } className="row">
+                        <svg width="30" height="30" viewBox="0 0 30 30" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M15 30C23.2843 30 30 23.2843 30 15C30 6.71573 23.2843 0 15 0C6.71573 0 0 6.71573 0 15C0 23.2843 6.71573 30 15 30ZM20.1693 12.2923C20.1693 15.1688 17.252 22.084 14.8773 22.084C12.5026 22.084 9.58594 15.1688 9.58594 12.2923C9.58594 9.41583 12.0008 7.08398 14.8773 7.08398C17.7538 7.08398 20.1693 9.41583 20.1693 12.2923ZM17.5 12.5C17.5 13.8807 16.3807 15 15 15C13.6193 15 12.5 13.8807 12.5 12.5C12.5 11.1193 13.6193 10 15 10C16.3807 10 17.5 11.1193 17.5 12.5Z" fill="#D5DDE0"/></svg>
+                        <span className="info-wrapper">
+                            <span className="address">{ address }</span>
+                            <span className="time-distance">
+                                <span>{ startTime }-{ endTime }</span>
+                                <span>{ this.getFormattedDistance(distance) }</span>
+                            </span>
+                        </span>
+                    </li>
         });
     }
 
@@ -29,7 +34,7 @@ class Table extends React.Component {
     render() {
         return (
             <div>
-                <table className='table'>
+                {/* <table className='table'>
                     <thead>
                         <tr>
                             <td>Time</td>
@@ -40,7 +45,13 @@ class Table extends React.Component {
                     <tbody>
                         { this.renderTable() }
                     </tbody>
-                </table>
+                </table> */}
+                <div>
+                    <ul className="list">
+                        { this.renderTable() }
+                    </ul>
+                </div>
+
             </div>
         );
     }
