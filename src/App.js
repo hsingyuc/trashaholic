ee57e91d3444548c81e2af2c8885a58664e7c071
Hsing Yu <hsingyuc.7@gmail.com>
2020-04-10 19:50:35 +0300

    Fix map zindex and button positioning

--- src/App.js
+++ src/App.js
@@ -130,6 +130,30 @@ class App extends React.Component {
     return (
       <div>
         <div className="main-section">
+          <button type="button" className="button-icon" onClick={() => this.setState( { isTableView: !isTableView } )}>
+            { isTableView
+              ? (
+                <svg width="40" height="40" viewBox="0 0 40 40" fill="none">
+                  <path d="M19.9751 28.5V18.75" stroke="#3E4958" strokeWidth="2" strokeLinecap="round" />
+                  <g filter="url(#filter0_d)">
+                    <path fillRule="evenodd" clipRule="evenodd" d="M19.9751 20.9165C22.6675 20.9165 24.8501 18.7339 24.8501 16.0415C24.8501 13.3491 22.6675 11.1665 19.9751 11.1665C17.2827 11.1665 15.1001 13.3491 15.1001 16.0415C15.1001 18.7339 17.2827 20.9165 19.9751 20.9165Z" fill="#1152FD" />
+                  </g>
+                  <ellipse cx="18.3502" cy="14.4168" rx="1.08333" ry="1.08333" fill="white" />
+                  <defs>
+                    <filter id="filter0_d" x="0.100098" y="0.166504" width="39.75" height="39.75" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                      <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
+                      <feOffset dy="4" />
+                      <feGaussianBlur stdDeviation="7.5" />
+                      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0" />
+                      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
+                      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
+                    </filter>
+                  </defs>
+                </svg>
+              )
+              : <FiMenu /> }
+          </button>
           { isTableView
             ? (
               <div className="table-wrapper">
@@ -140,27 +164,6 @@ class App extends React.Component {
                   setEndTime={( time ) => this.setState( { endTime: time } )}
                 />
                 <WasteTypes />
-                <button type="button" className="button-icon" onClick={() => this.setState( { isTableView: !isTableView } )}>
-                  <svg width="40" height="40" viewBox="0 0 40 40" fill="none">
-                    <path d="M19.9751 28.5V18.75" stroke="#3E4958" strokeWidth="2" strokeLinecap="round" />
-                    <g filter="url(#filter0_d)">
-                      <path fillRule="evenodd" clipRule="evenodd" d="M19.9751 20.9165C22.6675 20.9165 24.8501 18.7339 24.8501 16.0415C24.8501 13.3491 22.6675 11.1665 19.9751 11.1665C17.2827 11.1665 15.1001 13.3491 15.1001 16.0415C15.1001 18.7339 17.2827 20.9165 19.9751 20.9165Z" fill="#1152FD" />
-                    </g>
-                    <ellipse cx="18.3502" cy="14.4168" rx="1.08333" ry="1.08333" fill="white" />
-                    <defs>
-                      <filter id="filter0_d" x="0.100098" y="0.166504" width="39.75" height="39.75" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                        <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
-                        <feOffset dy="4" />
-                        <feGaussianBlur stdDeviation="7.5" />
-                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0" />
-                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
-                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
-                      </filter>
-                    </defs>
-                  </svg>
-                  { isTableView ? 'Show on a map' : <FiMenu /> }
-                </button>
                 <Table
                   collectionPlaces={filteredPlaces.slice( 0, 10 )}
                 />
